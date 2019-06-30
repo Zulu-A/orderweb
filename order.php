211ba9258100b77d4528bfb43e1d59510fc9a8ec
Zulu-A <alvin.githinji@strathmore.edu>
2019-06-30 22:41:13 +0300

    message

--- order.php
+++ order.php
@@ -12,9 +12,12 @@
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
        <style>
            .about-section {
+               position: absolute;
                min-height: 100%;
-               padding-top: 50px;
+               padding-top: 100px;
+               left: 5px;
                text-align: center;
+               
            }
            .head-section {
                min-height: 0px;
@@ -24,20 +27,18 @@
 
        </style>
    </head>
- <!-- ...................................<..................head...................>...................................................................   -->
-   
-   
+ <!-- ...................................<..................head navigation...................>...................................................................   -->
+ 
    <body>
       <section class="head-section">
           <header>
 
               <div class = "logo">
-                  <img src = "images/PM-logo-white.png" width ="200" title ="">
-                  <h1>PM FOODS</h1>
+                  <img src = "images/PM-logo-white.png" width ="100" title ="">
+                  <!-- <h1>PM FOODS</h1> -->
               </div>
 
               <nav class="navbar navbar-expand-lg " class="navbar navbar-light">
-                  <!-- <a class="navbar-brand" href="business.html">Switch to Business</a> -->
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                   </button>
@@ -58,87 +59,17 @@
 
           </header>
       </section>
-<!-- .......................................................................................................................................... -->
-      <!-- <div class="sidenav">
-         <div class="logo" id="text1" style=color:white;>
-            <h2>Order Now.</h2>
 
-            <div class = "logo">
-               <img src = "images/PM-logo-white.png" width ="200" title ="">
-               <h1>PM FOODS</h1>
-         </div>
-
-         </div>
-
-      </div> -->
-<!-- ......................................................................................................................................... -->
-      <!-- <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="adminfood-form">
-
-                <div class="form-group">
-                    <p id="msg1" class="alert"></p>
-                </div>
-
-                  <div class="form-group">
-                     <label>Food item:</label>
-                     <input type="checkbox" placeholder="Fooditem" id="finame">
-                  </div>
-
-                  <div class="form-group">
-                     <label id="fimage">Image</label>
-                  </div>
-                  
-                  <div class="form-group">
-                         <label>Quantity:</label>
-                         <input type="number" name="quantity" min="1" max="5" class="form-control" placeholder="quantity" id="quantity">
-                      </div>
-                      <div class="form-group">
-                        <label>Price: (Ksh)</label>
-                     </div>    
-                  <input type="submit" name="submit" value="Submit" class="btn btn-dark" style="color:orange;">
-            </div>
-         </div>
-      </div> -->
 <!-- ........................................order menu bootstrap cards............................................................... -->
       <section id="about" class="about-section">
           <div class="container">
               <div class="row">
-
-
-                  <!--<div class="card col-md-3" style="width: 18rem; margin-left: 10px;">
-                      <img class="card-img-top" src="images/beauburger.jpg" alt="Card image cap">
-                      <div class="card-body">
-                          <h5 class="card-title">Burger</h5>
-                          <p class="card-text">Price: Ksh. 300</p>
-                          <a href="#" class="btn btn-primary">Add to Cart</a>
-                      </div>
-                  </div>-->
 
                   <?php require 'php/checkItems.php'?>
 
               </div>
           </div>
       </section>
-
-      <!--<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="images/pizza eye.jpg" alt="Card image cap">
-         <div class="card-body">
-              <h5 class="card-title">Pizza</h5>
-              <p class="card-text">Pric: Ksh.400</p>
-              <a href="#" class="btn btn-primary">Add to Cart</a>
-         </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="images/fries eye.jpg" alt="Card image cap">
-         <div class="card-body">
-              <h5 class="card-title">Fries</h5>
-              <p class="card-text">Price: Ksh.200</p>
-              <a href="#" class="btn btn-primary">Add to Cart</a>
-         </div>
-      </div>-->
-<!-- ....................................................^...........^.......................................................................       -->
 
 
       <script type="text/javascript" src="cart-js/cart.js"></script>
@@ -160,6 +91,7 @@
 
 
       </script>
+<!-- ....................................................^...........^.......................................................................       -->
 
       </body>
    
